543b44e540a4b453b3d3a477b219dec2c8e0fa3a
artcevvv <artcevvv@yandex.kz>
2024-10-10 15:46:44 +0500

    qwer

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	// Load environment variables
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("Error loading dotenv: %s\n", err)
@@ -18,30 +19,32 @@ func main() {
 
 	bot_token := os.Getenv("TELEGRAM_TOKEN")
 
+	// Create a new bot instance
 	bot, err := telego.NewBot(bot_token)
-
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	botUser, _ := bot.GetMe()
-
 	fmt.Printf("Bot user: %+v\n", botUser)
+
+	// Get updates via long polling
 	updates, _ := bot.UpdatesViaLongPolling(nil)
 
+	// Create bot handler
 	bh, _ := th.NewBotHandler(bot, updates)
 
 	defer bh.Stop()
 	defer bot.StopLongPolling()
 
-	for update := range updates {
+	// Handle the "call" command
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		// Only process group or supergroup chats
 		if update.Message != nil && (update.Message.Chat.Type == "group" || update.Message.Chat.Type == "supergroup") {
 			chatID := update.Message.Chat.ID
 
-			chatIdentifier := telego.ChatID{
-				ID: chatID,
-			}
-
+			// Get chat administrators for the current chat
+			chatIdentifier := telego.ChatID{ID: chatID}
 			admins, err := bot.GetChatAdministrators(&telego.GetChatAdministratorsParams{
 				ChatID: chatIdentifier,
 			})
@@ -52,26 +55,18 @@ func main() {
 			}
 
 			var adminList []string
-
 			for _, admin := range admins {
 				if admin.MemberUser().Username != "caller_BDA_bot" && admin.MemberUser().Username != "" {
 					adminList = append(adminList, "@"+admin.MemberUser().Username)
-				} else {
-					continue
 				}
 			}
 
-			bh.Handle(func(bot *telego.Bot, updates telego.Update) {
-				messageText := "Calling everyone... \n" + fmt.Sprintf("%v", adminList)
-				_, _ = bot.SendMessage(
-					tu.Message(
-						tu.ID(chatID),
-						messageText,
-					),
-				)
-			}, th.CommandEqual("call"))
-
-			bh.Start()
+			// Send the message with the list of admins in the current chat
+			messageText := "Calling everyone... \n" + fmt.Sprintf("%v", adminList)
+			_, _ = bot.SendMessage(tu.Message(tu.ID(chatID), messageText))
 		}
-	}
+	}, th.CommandEqual("call"))
+
+	// Start the bot handler
+	bh.Start()
 }
