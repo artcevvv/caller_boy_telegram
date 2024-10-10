d41db16769641b0e0826bdedc0da2b2a3adfc134
artcevvv <artcevvv@yandex.kz>
2024-10-10 15:53:24 +0500

    qwer

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 )
 
 func main() {
-	// Load environment variables
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("Error loading dotenv: %s\n", err)
@@ -19,7 +18,6 @@ func main() {
 
 	bot_token := os.Getenv("TELEGRAM_TOKEN")
 
-	// Create a new bot instance
 	bot, err := telego.NewBot(bot_token)
 	if err != nil {
 		fmt.Println(err)
@@ -28,22 +26,17 @@ func main() {
 	botUser, _ := bot.GetMe()
 	fmt.Printf("Bot user: %+v\n", botUser)
 
-	// Get updates via long polling
 	updates, _ := bot.UpdatesViaLongPolling(nil)
 
-	// Create bot handler
 	bh, _ := th.NewBotHandler(bot, updates)
 
 	defer bh.Stop()
 	defer bot.StopLongPolling()
 
-	// Handle the "call" command
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		// Only process group or supergroup chats
 		if update.Message != nil && (update.Message.Chat.Type == "group" || update.Message.Chat.Type == "supergroup") {
 			chatID := update.Message.Chat.ID
 
-			// Get chat administrators for the current chat
 			chatIdentifier := telego.ChatID{ID: chatID}
 			admins, err := bot.GetChatAdministrators(&telego.GetChatAdministratorsParams{
 				ChatID: chatIdentifier,
@@ -61,12 +54,10 @@ func main() {
 				}
 			}
 
-			// Send the message with the list of admins in the current chat
 			messageText := "Calling everyone... \n" + fmt.Sprintf("%v", adminList)
 			_, _ = bot.SendMessage(tu.Message(tu.ID(chatID), messageText))
 		}
 	}, th.CommandEqual("call"))
 
-	// Start the bot handler
 	bh.Start()
 }
